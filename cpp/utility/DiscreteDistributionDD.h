7e0001feeb792340d1546497bbdc1f6c26c5b400
Tianyi Gu <2008tygu@gmail.com>
2019-08-29 14:29:13 -0400

    try smoothed data for heavy

--- cpp/utility/DiscreteDistributionDD.h
+++ cpp/utility/DiscreteDistributionDD.h
@@ -111,13 +111,13 @@ public:
         string fileName = "/home/aifs1/gu/phd/research/workingPaper/"
                           "realtime-nancy/results/SlidingTilePuzzle/"
                           "sampleData/" +
-                domain.getSubDomainName() + "-statSummary-nomissing.txt";
+                domain.getSubDomainName() + "-statSummary-nomissing-smoothed.txt";
 
         string fileName_ps = "/home/aifs1/gu/phd/research/workingPaper/"
                              "realtime-nancy/results/SlidingTilePuzzle/"
                              "sampleData/" +
                 domain.getSubDomainName() +
-                "-statSummary-postSearch-nomissing.txt";
+                "-statSummary-postSearch-nomissing-smoothed.txt";
 
         ifstream f(fileName);
         ifstream f_ps(fileName_ps);
