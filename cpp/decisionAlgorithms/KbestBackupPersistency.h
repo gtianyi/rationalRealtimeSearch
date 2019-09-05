6073381f3beed1c072fa62d5b1f6a97a615fadbe
Tianyi Gu <2008tygu@gmail.com>
2019-09-05 16:53:09 -0400

    trying to fix persistency for asumption-based nancy, but not work well

--- cpp/decisionAlgorithms/KbestBackupPersistency.h
+++ cpp/decisionAlgorithms/KbestBackupPersistency.h
@@ -4,13 +4,15 @@
 #include <iostream>
 #include <memory>
 #include <stack>
+#include <cassert>
 #include "../utility/PriorityQueue.h"
 #include "KBestBackup.h"
 
 using namespace std;
 
 template <class Domain, class Node, class TopLevelAction>
-class KBestBackupPersistency : public KBestBackup<Domain, Node, TopLevelAction> {
+class KBestBackupPersistency
+        : public KBestBackup<Domain, Node, TopLevelAction> {
     typedef typename Domain::State State;
     typedef typename Domain::Cost Cost;
     typedef typename Domain::HashState Hash;
@@ -33,25 +35,26 @@ public:
                 lowestExpectedPathTLA = tla;
         }
 
-        shared_ptr<Node> goalPrime = lowestExpectedPathTLA->topLevelNode;
 
         auto tlaBest = lowestExpectedPathTLA->open_TLA.top();
 
-        if (persistPath.empty() ||
-                lowestExpectedPathTLA->expectedMinimumPathCost < persistFhat) {
+        if (!persistPath.empty())
+            updatePersistFhat();
+
+        auto newfhat = lowestExpectedPathTLA->expectedMinimumPathCost;
+
+        if (persistPath.empty() || newfhat < persistFhat) {
             // if there is no persist path, go head memoize it
             // if we find a better fhat for root, go head memoize it
             memoizePersistPath(lowestExpectedPathTLA);
-        } else if (lowestExpectedPathTLA->expectedMinimumPathCost ==
-                        persistFhat &&
-                tlaBest->getHHatValueFromDist() <=
-                        persistTarget->getHHatValueFromDist()) {
-            // if  fhat ties break ties on h-hat then prefer not persist
+        } else if (newfhat == persistFhat &&
+                tlaBest->getHHatValue() <= persistTarget->getHHatValue()) {
+            // if  fhat ties break ties on h-hat, then prefer not persist
             memoizePersistPath(lowestExpectedPathTLA);
-        } else{
-            // if we find a worse fhat, but previous target is inside LSS, 
-			// we then still want to memoize it because the learning then 
-			// will update the previous target.
+        } else {
+            // if we find a worse fhat, but previous target is inside LSS,
+            // we then still want to memoize it because the learning
+            // will update the previous target.
             auto it = closed.find(persistTarget->getState());
             if (it != closed.end() && !it->second->onOpen()) {
                 memoizePersistPath(lowestExpectedPathTLA);
@@ -59,39 +62,65 @@ public:
                 cout << "persist" << endl;
                 cout << "previous fhat " << persistFhat << endl;
                 cout << "new fhat "
-                     << lowestExpectedPathTLA->expectedMinimumPathCost +
-                                lowestExpectedPathTLA->topLevelNode->getGValue()
-                     << endl;
-			}
+                     << lowestExpectedPathTLA->expectedMinimumPathCost << endl;
+            }
         }
 
-        goalPrime = persistPath.top();
-		persistPath.pop();
-		persistFhat -= goalPrime->getGValue();
+        shared_ptr<Node> goalPrime = persistPath.top();
+        auto goalPrimeG = persistG.top();
+
+        persistPath.pop();
+        persistG.pop();
+
+		// take out that small g at top of the chain
+        persistFhat = persistFhat - goalPrimeG;
+
+        prevTargetHHAT = persistTarget->getHHatValue();
 
         return goalPrime;
     }
 
 private:
     void memoizePersistPath(shared_ptr<TopLevelAction> tla) {
-		//clear the persist path
+        // clear the persist path
         while (!persistPath.empty()) {
             persistPath.pop();
+            persistG.pop();
         }
+
+        assert(persistG.empty());
+        assert(persistPath.empty());
 
         auto cur = tla->open_TLA.top();
 
         while (cur->getParent() != nullptr) {
             persistPath.push(cur);
+
+            auto curG = cur->getGValue();
+
             cur = cur->getParent();
+
+            auto parentG = cur->getGValue();
+            persistG.push(curG - parentG);
         }
+
+        assert(persistG.size() == persistPath.size());
 
         persistTarget = tla->open_TLA.top();
         persistFhat = tla->expectedMinimumPathCost;
     }
 
+    void updatePersistFhat() {
+        persistTarget->setEpsilonH(this->domain.epsilonHGlobal());
+        persistTarget->setEpsilonD(this->domain.epsilonDGlobal());
+        persistFhat =
+                persistFhat - prevTargetHHAT + persistTarget->getHHatValue();
+    }
+
 protected:
     std::stack<shared_ptr<Node>> persistPath;
+    std::stack<Cost> persistG;
     shared_ptr<Node> persistTarget;
     Cost persistFhat;
+    Cost prevTargetHHAT;
 };
