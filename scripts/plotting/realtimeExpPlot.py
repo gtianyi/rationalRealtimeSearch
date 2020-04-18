6aa320d54e40f58becd3927f706aa2c6f52ab339
Tianyi Gu <2008tygu@gmail.com>
2020-04-18 10:10:07 -0400

    updating ploting script to support pancake, but argument parse is still require manul effot

--- scripts/plotting/realtimeExpPlot.py
+++ scripts/plotting/realtimeExpPlot.py
@@ -4,6 +4,7 @@ plotting code for generate nancydd related plots
 Author: Tianyi Gu
 Date: 08/15/2019
 Update: 10/15/2019
+Update: 04/18/2020
 '''
 
 #!/usr/bin/env python
@@ -12,11 +13,12 @@ __author__ = 'tianyigu'
 
 import json
 from os import listdir
+from os import mkdir
 
 import sys
 from datetime import datetime
 from collections import OrderedDict
-iemport matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -54,8 +56,8 @@ def makeDifferencePlot(width, height, xAxis, yAxis, dataframe, dodge, hue,
     plt.ylabel(yLabel, color='black', fontsize=18)
     plt.xlabel(xLabel, color='black', fontsize=18)
 
-    plt.setp(ax.get_legend().get_texts(), fontsize='18') # for legend text
-    plt.setp(ax.get_legend().get_title(), fontsize='18') # for legend title
+    plt.setp(ax.get_legend().get_texts(), fontsize='18')  # for legend text
+    plt.setp(ax.get_legend().get_title(), fontsize='18')  # for legend title
 
     plt.savefig(outputName, bbox_inches="tight", pad_inches=0)
     plt.close()
@@ -68,11 +70,12 @@ def makeCoverageTable(dataframe, subdomainType):
     grp = dataframe.groupby(['Node Expansion Limit',
                              'Algorithm'])['Solution Cost'].count()
     print(grp)
-    grp.to_csv("../../plots/" + subdomainType + "/coverage-" + subdomainType + ".csv")
+    grp.to_csv("../../plots/" + subdomainType + "/coverage-" + subdomainType +
+               ".csv")
 
 
 def main():
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 4:
         printUsage()
         return
 
@@ -82,8 +85,8 @@ def main():
     ]
 
     # Hard coded result directories
-    tileDimension = "16"
-    # tileDimension = "4x4"
+    domainSize = "32"
+    # domainSize = "4x4"
     domainType = sys.argv[2]
     subdomainType = sys.argv[3]
     limits = [3, 10, 30, 100, 300, 1000]
@@ -162,8 +165,17 @@ def main():
     })
 
     #specify the order for camera ready of AAAI-20
-    # algorithm_order = ['Nancy (DD)', 'Nancy (DD NewPer)', 'Nancy (Assumption-DD NewPer)', 'LSS-LRTA*', 'Nancy (pers.)',  'Nancy', 'Nancy-hhat', 'Nancy (pers-fix-assumption.)', 'Nancy (pers-fix-assumption-hhat.)', 'Nancy (pers-hhat.)']
-    algorithm_order = ['Nancy (DD)',  'LSS-LRTA*', 'Nancy (pers.)', ]
+    # algorithm_order = [
+    # 'Nancy (DD)', 'Nancy (DD NewPer)', 'Nancy (Assumption-DD NewPer)',
+    # 'LSS-LRTA*', 'Nancy (pers.)', 'Nancy', 'Nancy-hhat',
+    # 'Nancy (pers-fix-assumption.)', 'Nancy (pers-fix-assumption-hhat.)',
+    # 'Nancy (pers-hhat.)'
+    # ]
+    algorithm_order = [
+        'Nancy (DD)',
+        'LSS-LRTA*',
+        'Nancy (pers.)',
+    ]
 
     baseline = "LSS-LRTA*"
 
@@ -178,15 +190,15 @@ def main():
     for alg in algorithms:
         for jsonFile in listdir(
                 # "../../results/SlidingTilePuzzle/expansionTests/NancyDD/" +
-                "../../results/"+domainType+"/expansionTests/NancyDD/" +
-                subdomainType + '/' + alg + '/' + tileDimension):
+                "../../results/" + domainType + "/expansionTests/NancyDD/" +
+                subdomainType + '/' + alg + '/' + domainSize):
             if jsonFile[-5:] != ".json":
                 continue
             with open(
                     # "../../results/SlidingTilePuzzle/expansionTests/NancyDD/" +
-                    "../../results/"+domainType+"/expansionTests/NancyDD/" +
-                    subdomainType + '/' + alg + '/' + tileDimension + "/" +
-                    jsonFile) as json_data:
+                    "../../results/" + domainType +
+                    "/expansionTests/NancyDD/" + subdomainType + '/' + alg +
+                    '/' + domainSize + "/" + jsonFile) as json_data:
 
                 resultData = json.load(json_data)
                 if float(resultData[algorithms_data[alg]]) != -1.0:
@@ -232,24 +244,27 @@ def main():
 
     nowstr = datetime.now().strftime("%d%m%Y-%H%M")
 
+    out_dir = "../../plots/" + domainType
+    mkdir(out_dir)
+    out_file = out_dir + '/' + domainType + "-" + subdomainType + "-" + domainSize + '-' + nowstr
+
     if sys.argv[1] == "coverage":
         makeCoverageTable(rawdf, subdomainType)
 
     elif sys.argv[1] == "pairwise":
-        makeDifferencePlot(
-            13, 10, "Node Expansion Limit",
-            "Algorithm Cost - " + baseline + " Cost", df, 0.35, "Algorithm",
-            limits, algorithm_order, "Node Expansion Limit",
-            "Algorithm Cost - " + baseline + " Cost", "../../plots/" + domainType + '/' +
-            subdomainType + '/' + subdomainType + "-tile-pairwise-" + nowstr + ".eps",
-            markers)
+        makeDifferencePlot(13, 10, "Node Expansion Limit",
+                           "Algorithm Cost - " + baseline + " Cost", df, 0.35,
+                           "Algorithm", limits, algorithm_order,
+                           "Node Expansion Limit",
+                           "Algorithm Cost - " + baseline + " Cost",
+                           out_file + "-pairwise.eps", markers)
 
     elif sys.argv[1] == "solutioncost":
-        makeDifferencePlot(
-            13, 10, "Node Expansion Limit", "Solution Cost", df, 0.35,
-            "Algorithm", limits, algorithms.values(), "Node Expansion Limit",
-            "Solution Cost", "../../plots/" + domainType + '/' +subdomainType + '/' + subdomainType +
-            "-tile-solution-cost-" + nowstr + ".png", markers)
+        makeDifferencePlot(13, 10, "Node Expansion Limit",
+                           "Solution Cost", df, 0.35, "Algorithm", limits,
+                           algorithms.values(), "Node Expansion Limit",
+                           "Solution Cost", out_file + "-solutioncost.png",
+                           markers)
     else:
         printUsage()
 
